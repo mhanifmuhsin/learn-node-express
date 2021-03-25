a57d77c1c25f4636f69e726e34ab7728a319f96d
mhanifmuhsin <m.hanif.muhsin@gmail.com>
2021-03-25 14:43:17 +0700

    route use req.body

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
 const express = require('express');
-const routers = require('./routers')
+const routers = require('./routers');
 const app = express();
-const port = 3000
+const port = 3000;
 
 // app.METHOD(ROUTE, CALLBACK)
-// app.get('/',(req,res) => {
-//     res.send('Hello World')
-// });
+app.get('/',(req,res) => {
+    res.send('Hello World')
+});
 
 // app.get('/post/:id',(req,res) => {
 //     res.send('artikel-'+req.params.id)
 // });
 
-app.use(routers)
+//contoh middleware 1
+// const log = (req,res,next) =>{
+//     console.log(Date.now() + '' +req.ip+ '' +req.originalUrl)
+//     next()
+// };
 
-app.listen(port,()=> console.log(`server running at http://localhost:${port}`));
+//contoh middleware 2
+// const log = (req,res,next) =>{
+//     res.status(404).send('resource tidak ditemukan');
+// }
+
+// contoh middleware 3 404 routers tidak ditemukan
+const log = (req, res, next) => {
+    res.json({
+        status: 'error',
+        message: 'resource tidak ditemukan'
+    })
+}
+
+// contoh middleware handling error 500
+const handleError = (err, req, res, next) => {
+    res.json({
+        status: 'error',
+        message: 'terjadi kesalahan server'
+    })
+}
+
+
+
+
+// app.use(handleError)
+// app.use(log)
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+// penempatan mempengaruhi
+app.use(routers)
+app.listen(port, () => console.log(`server running at http://localhost:${port}`));
 
--- routers.js
+++ routers.js
@@ -9,4 +9,27 @@ routers.get('/post/:id',(req,res) => {
     res.send('artikel-'+req.params.id)
 });
 
+// routers.post('/login',(req,res) =>{
+//     const {username, password} = req.body
+//     res.send(`Anda login dengan username : ${username} dan password : ${password}`)
+//     // res.send(`Anda login dengan username `)
+
+// })
+routers.post('/login',(req, res) => {
+    const { username, password } = req.body
+    // var username = req.body.username;
+    // var password = req.body.password;
+    res.send(`Anda login dengan username ${username} dan password
+    ${password}`) })
+    
+
+routers.post('/api/users', function(req, res) {
+    var user_id = req.body.id;
+    var token = req.body.token;
+    var geo = req.body.geo;
+
+    res.send(user_id + ' ' + token + ' ' + geo);
+});
+   
+
 module.exports = routers
